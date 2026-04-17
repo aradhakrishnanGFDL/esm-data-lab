b4255030fe143a2f5f4b4db9968a4c023072e44a
aradhakrishnanGFDL <aparna.radhakrishnan@noaa.gov>
2026-04-17 15:27:17 -0400

    ide placeholder

--- modules/ide.py
+++ modules/ide.py
@@ -1,1 +1,2 @@
 import streamlit as st
+st.write("This module is under construction. In the meantime, you can start working on your diagnostics in your IDE and push changes to GitHub using the workflow you learned in the previous module. If you have any questions, please reach out to us.")
