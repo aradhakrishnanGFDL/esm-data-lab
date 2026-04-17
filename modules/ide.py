cd865d8f9f2ea7187f32bf2511ff2d210ce6a91d
aradhakrishnanGFDL <aparna.radhakrishnan@noaa.gov>
2026-04-17 15:32:07 -0400

    ide run added

--- modules/ide.py
+++ modules/ide.py
@@ -1,2 +1,4 @@
 import streamlit as st
-st.write("This module is under construction. In the meantime, you can start working on your diagnostics in your IDE and push changes to GitHub using the workflow you learned in the previous module. If you have any questions, please reach out to us.")
+def run():
+  st.write("This module is under construction. In the meantime, you can start working on your diagnostics in your IDE and push changes to GitHub using the workflow you learned in the previous module. If you have any questions, please reach out to us.")
+  
