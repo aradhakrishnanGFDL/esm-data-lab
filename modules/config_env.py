7e943064a448e2014d7fd1cca53922c60dea1e24
aradhakrishnanGFDL <aparna.radhakrishnan@noaa.gov>
2026-05-11 16:14:12 -0400

    placeholders

---
+++ modules/config_env.py
@@ -0,0 +1,4 @@
+import streamlit as st
+def run():
+  st.write("This module is under construction. In the meantime, you can start working on your diagnostics in your IDE and push changes to GitHub using the workflow you learned in the previous module. If you have any questions, please reach out to us.")
+  
